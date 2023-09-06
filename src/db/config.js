9c5345d2a460939e1941a49dbec21831379bb29a
balu <nbkrishna32@gmail.com>
2023-09-06 11:33:27 +0530

    changes in .gitignore

---
+++ src/db/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // host: '201.161.178.68',
+    // user: 'demoteks_teks',
+    // password: 'Tek$academy@1',
+    // database: 'demoteks_teks',
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test',
+};
